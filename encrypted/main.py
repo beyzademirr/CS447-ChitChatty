dc8db6072abd5ad1cfc1616237773ebd72da471d
beyzademirr <92509000+beyzademirr@users.noreply.github.com>
2023-01-05 14:28:17 +0300

    Update main.py

--- encrypted/main.py
+++ encrypted/main.py
@@ -3,6 +3,8 @@ import threading
 import rsa
 
 choice = input("(1) or (2):")
+public_key, private_key = rsa.newkeys(1024)
+public_partner = None
 
 if choice == "1":
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,10 +12,15 @@ if choice == "1":
     server.listen()
 
     client, _ = server.accept()
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
 
 elif choice == "2":
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("172.24.128.1", 55555))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+    client.send(public_key.save_pkcs1("PEM"))
+
 else:
     exit()
 
@@ -22,12 +29,12 @@ else:
 def sending_messages(msg):
     while True:
         message = input("")
-        msg.send(message.encode())
+        msg.send(rsa.encrypt(message.encode(), public_partner))
         print("You: " + message)
 
 def receiving_messages(msg):
     while True:       
-        print("Partner: " + msg.recv(1024).decode())
+        print("Partner: " + rsa.decrypt(msg.recv(1024), private_key).decode())
 
 threading.Thread(target=sending_messages, args=(client,)).start()
 threading.Thread(target=receiving_messages, args=(client,)).start()
