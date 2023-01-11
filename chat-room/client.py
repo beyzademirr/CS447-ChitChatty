080bd2c0c044ce50eee34ddd79ea725fca0f8651
doga.sezgin <doga.sezgin@ozu.edu.tr>
2023-01-11 16:02:23 +0300

    stop_event added trial

--- chat-room/client.py
+++ chat-room/client.py
@@ -10,12 +10,13 @@ public_partner = None
 
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
-
+client.connect(('10.200.23.63', 3389))
+stop_event = threading.Event()
 # Listening to Server and Sending Nickname
 def receive():
     global public_partner
-    while True:
+
+    while not stop_event.is_set():
         try:
             # Receive Message From Server
             # If 'NICK' Send Nickname
@@ -39,11 +40,15 @@ def receive():
 
 # Sending Messages To Server
 def write():
-    while True:
+    while not stop_event.is_set():
         message = input('')
         
         client.send(rsa.encrypt(message.encode(), public_partner))
         print("You: " + message)
+
+def close_chat():
+    stop_event.is_set()
+    client.close()
 
 
 # Starting Threads For Listening And Writing
