8c9dcffafe42396dea44c7ada39af6ddabaa5966
doga.sezgin <doga.sezgin@ozu.edu.tr>
2023-01-12 21:48:49 +0300

    gui added without full functionality

--- e2e-multiple-clients/client.py
+++ e2e-multiple-clients/client.py
@@ -1,6 +1,14 @@
 import socket
 import threading
+from tkinter import END
+
 import rsa
+import tkinter as tk
+
+
+
+
+
 
 # Choosing Nickname
 nickname = input("Choose your nickname: ")
@@ -10,13 +18,25 @@ public_partner = None
 
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1'
-, 65535))
+client.connect(('127.0.0.1', 65535))
+stop_event = threading.Event()
 
+#GUI
+root = tk.Tk()
+root.title("ChitChatty of " + nickname)
+root.geometry("500x600")
+label = tk.Label(root, text="CHITCHATTY", font=('Arial', 30))
+label.pack(padx=30, pady=30)
+messages = tk.Text(root, width=50, padx=30, pady=30, borderwidth=2, bg="gray")
+messages.pack()
+
+yourmessage = tk.Entry(root, width=50, bg="yellow")
+yourmessage.insert(0, "Write a message..")
+yourmessage.pack()
 # Listening to Server and Sending Nickname
 def receive():
     global public_partner
-    while True:
+    while not stop_event.set():
         try:
             # Receive Message From Server
             # If 'NICK' Send Nickname
@@ -29,9 +49,7 @@ def receive():
                 public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
                 print("Your partner has joined")
             elif message == 'MESSAGE':
-                print("Partner: " + rsa.decrypt(client.recv(1024), private_key).decode())
-            elif message == 'CLOSE':
-                client.close()
+                print("Partner: " + rsa.decrypt(client.recv(1024), private_key).decode('ascii'))
             else:
                 print(message)
         except Exception as e:
@@ -43,7 +61,7 @@ def receive():
 
 # Sending Messages To Server
 def write():
-    while True:
+    while not stop_event.set():
     
         message = input('')
 
@@ -52,8 +70,10 @@ def write():
          print("You: " + message)
         else:
             print("Wait for your partner")
-        
-        
+
+def close_chat():
+    stop_event.set()
+    client.close()
 
 
 # Starting Threads For Listening And Writing
@@ -62,3 +82,6 @@ receive_thread.start()
 
 write_thread = threading.Thread(target=write)
 write_thread.start()
+
+root.mainloop()
+
