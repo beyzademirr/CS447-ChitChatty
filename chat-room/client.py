6ce6a2ef15cf20048bb4178322bec89b5f8938f5
sezinsayan <sezin.sayan@ozu.edu.tr>
2023-01-12 19:14:00 +0300

    group chat added.

--- chat-room/client.py
+++ chat-room/client.py
@@ -1,59 +1,39 @@
 import socket
 import threading
-import rsa
 
-# Choosing Nickname
+# choosing nickname
 nickname = input("Choose your nickname: ")
 
-public_key, private_key = rsa.newkeys(1024)
-public_partner = None
-
-# Connecting To Server
+# connecting to server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('10.200.23.63', 3389))
-stop_event = threading.Event()
-# Listening to Server and Sending Nickname
-def receive():
-    global public_partner
+client.connect(('127.0.0.1', 65500))
 
-    while not stop_event.is_set():
+def receive():
+    while True:
         try:
-            # Receive Message From Server
-            # If 'NICK' Send Nickname
             message = client.recv(1024).decode('ascii')
             if message == 'NICK':
                 client.send(nickname.encode('ascii'))
-            elif message == 'KEY':
-                client.send(public_key.save_pkcs1("PEM"))
-            elif message == 'PARTNER':
-                public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
-            elif message == 'MESSAGE':
-                print("Partner: " + rsa.decrypt(client.recv(1024), private_key).decode()) 
             else:
                 print(message)
-        except Exception as e:
-            # Close Connection When Error
-            print(e)
+        except:
             print("An error occured!")
             client.close()
             break
 
-# Sending Messages To Server
+# sending message to server
 def write():
-    while not stop_event.is_set():
-        message = input('')
-        
-        client.send(rsa.encrypt(message.encode(), public_partner))
-        print("You: " + message)
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
 
-def close_chat():
-    stop_event.is_set()
-    client.close()
-
-
-# Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
 write_thread = threading.Thread(target=write)
 write_thread.start()
+
+# Wait for the threads to finish execution before client close
+receive_thread.join()
+write_thread.join()
+client.close()
--- chat-room/server.py
+++ chat-room/server.py
@@ -1,11 +1,10 @@
 import socket
 import threading
-import rsa
 
 
 # Connection Data
-host = ''
-port = 55555
+host = '127.0.0.1'
+port = 65500
 
 # Starting Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,21 +14,11 @@ server.listen()
 # Lists For Clients and Their Nicknames
 clients = []
 nicknames = []
-keys = []
 
 # Sending Messages To All Connected Clients
-def broadcast(message, client0):
-    if len(clients)==2:
-        for client in clients:
-            if client0!=client:
-                client.send('MESSAGE'.encode('ascii'))
-                client.send(message)
-
-# Sending Messages To All Connected Clients
-def broadcast2(message):
-    if len(clients)==2:
-        for client in clients:
-            client.send(message)
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
 # Handling Messages From Clients
 def handle(client):
@@ -37,20 +26,14 @@ def handle(client):
         try:
             # Broadcasting Messages
             message = client.recv(1024)
-            if len(clients)==2:
-                if(client==clients[0]):
-                    clients[1].send('MESSAGE'.encode('ascii'))
-                    clients[1].send(message)
-                else:
-                    clients[0].send('MESSAGE'.encode('ascii'))
-                    clients[0].send(message)
+            broadcast(message)
         except:
             # Removing And Closing Clients
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            #broadcast('{} left!'.format(nickname).encode('ascii'))
+            broadcast('{} left!'.format(nickname).encode('ascii'))
             nicknames.remove(nickname)
             break
 
@@ -64,34 +47,12 @@ def receive():
         # Request And Store Nickname
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
-        client.send('KEY'.encode('ascii'))
-        key = rsa.PublicKey.load_pkcs1(client.recv(1024))
-        
-       
-        keys.append(key)
         nicknames.append(nickname)
         clients.append(client)
 
-        if(len(clients)==2):
-            print("AAAAAAAAAAAAAAAAAAAAAAAAA")
-            client.send('PARTNER'.encode('ascii'))
-            client.send(keys[0].save_pkcs1("PEM"))
-            #client.send('PARTNER-NAME'.encode('ascii'))
-            #client.send(nicknames[0].encode())
-            
-            client2 = clients[0]
-            client2.send('PARTNER'.encode('ascii'))
-            client2.send(keys[1].save_pkcs1("PEM"))
-            #client2.send('PARTNER-NAME'.encode('ascii'))
-            #client2.send(nicknames[1].encode())
-        else:
-            print()
-            
-
-
         # Print And Broadcast Nickname
         print("Nickname is {}".format(nickname))
-        broadcast2("{} joined!".format(nickname).encode('ascii'))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
         client.send('Connected to server!'.encode('ascii'))
 
         # Start Handling Thread For Client
