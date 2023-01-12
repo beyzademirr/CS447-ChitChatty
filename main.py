22ff0a4d97ae492e3eea2a1c40f9144a33936c85
sezinsayan <sezin.sayan@ozu.edu.tr>
2023-01-12 22:31:37 +0300

    main to implement both

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-import subprocess
 import rsa
+import subprocess
+
+host = '127.0.0.1'
 choice = input("Private Chat(1) or Group Chat(2):")
+
 public_key, private_key = rsa.newkeys(1024)
 public_partner = None
 
-#private chat with multiple clients
-if choice == "1":
-    subprocess.run(["/opt/homebrew/bin/python3", "e2e-multiple-clients/client.py"])
+if host == '127.0.0.1':
+    if choice == "1":
+        server=subprocess.Popen(["python3", "e2e-multiple-clients/server.py"])
+    elif choice == "2":
+        server=subprocess.Popen(["python", "chat-room/server.py"])
+    else:
+        exit()
 
-#group chat
+if choice == "1":
+    client = subprocess.Popen(["python3", "e2e-multiple-clients/client.py"])
 elif choice == "2":
-   subprocess.run(["python", "chat-room/client.py"])
+    client = subprocess.Popen(["python", "chat-room/client.py"])
 else:
     exit()
 
-
-
-
-
+server.wait()
+client.wait()
