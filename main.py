2cd05c6f0047aef4011e30d44c774a9f95300c2f
sezinsayan <sezin.sayan@ozu.edu.tr>
2023-01-12 19:15:50 +0300

    main updated again.

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ choice = input("Private Chat(1) or Group Chat(2):")
 public_key, private_key = rsa.newkeys(1024)
 public_partner = None
 
+#private chat with multiple clients
 if choice == "1":
-    subprocess.run(["/opt/homebrew/bin/python3", "chat-room/client.py"])
+    subprocess.run(["/opt/homebrew/bin/python3", "e2e-multiple-clients/client.py"])
 
+#group chat
 elif choice == "2":
-   subprocess.run(["python", "e2e-multiple-clients/client.py"])
+   subprocess.run(["python", "chat-room/client.py"])
 else:
     exit()
 
